f2509a4dbfb245686a7151591790199c91c7150e
nlensk <72632056+nlensk@users.noreply.github.com>
2026-03-17 16:31:20 -0400

    Create genome_string.py

---
+++ Code/genome_string.py
@@ -0,0 +1,31 @@
+"""
+@author: Nadia Lenskaia 2025
+All rights reserved.
+"""
+# This code creates a dictionary where the headers are the keys and the sequences are the values
+
+def genomestring(t):
+    header = t[0]
+    header = header.strip()
+    header = header.split(" |")
+    header = header[0]
+    header = header[1:]
+
+    t = t[1:]
+    genome = ""
+    
+    for line in t:
+        #ff.write(line)
+        line = line.strip()
+        genome += lin    
+        
+    return [header, genome]
+    
+
+fname = "example.fasta"
+f = open(fname, "r")
+t1 = f.readlines()
+f.close()
+
+res = genomestring(t1)
+print(res[0], len(res[1]))
